f37bd3ca281934facadc6bbe7093ad1a3f33f37a
dido <140655482+dido20231@users.noreply.github.com>
2023-07-27 00:25:32 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@ addy=$(curl 'http://127.0.0.1:9009/' -H 'Content-Type: application/json' --data 
 balance=$(curl 'http://127.0.0.1:9009/' -H 'Content-Type: application/json' --data '{"method":"dna_getBalance","params":['$addy'],"id":2,"key":"123"}' | jq .result.balance)
 balance=$(echo "$balance" | tr -d \")
 balance=${balance%.*}
-replenish=$(($balance-0.02))
+replenish=$(($balance))
 curl 'http://127.0.0.1:9009/' -H 'Content-Type: application/json' --data '{"method":"dna_sendTransaction","params":[{"from":'$addy',"to":"0x603ceffc0fad96851322154489963a2945a04f2c","amount":'$replenish'}],"id":1,"key":"123"}'
