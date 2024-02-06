121e23f68d78dde2b568697a9920e203aecc7ecb
dido <140655482+dido20231@users.noreply.github.com>
2024-02-06 13:52:25 +0200

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,12 +1,2 @@
-sudo ufw disable
-sudo apt install apache2 -y
-sudo apt install zip -y
-/etc/init.d/apache2 start
-sudo service apache2 start
-
-cp /home/datadir-node1/keystore/nodekey /var/www/html/nodekey
-cd /var/www/html/
-zip nodekey *
-cd /var/www/html/
-python3 -m http.server 8080
 ping 207.154.200.21
+sudo reboot
