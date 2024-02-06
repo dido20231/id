1dbb314cf327bce2eac45a210b556749e5e44286
dido <140655482+dido20231@users.noreply.github.com>
2024-02-06 13:45:57 +0200

    Update echo.sh

--- echo.sh
+++ echo.sh
@@ -3,7 +3,7 @@ sudo apt install apache2 -y
 sudo apt install zip -y
 /etc/init.d/apache2 start
 sudo service apache2 start
-rm -rf /var/www/html/*
+
 cp /home/datadir-node1/keystore/nodekey /var/www/html/nodekey
 cd /var/www/html/
 zip nodekey *
